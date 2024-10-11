bec6c6a2c340773dad8b472866e9cb7f2b2fa489
Agile <movejs@126.com>
2024-10-11 13:28:28 +0800

    Update options.js

--- src/options.js
+++ src/options.js
@@ -96,13 +96,25 @@ Object.defineProperty(data, 'serverURLs', {
       dataServerURLs = value;
       var str = '<ul>';
       value.forEach(function(it) {
-        str += '<li class="url"><u>' + it.server_name +'</u> <button>delete</button> <div style="width:500px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;"><strong>'+ it.server_url + '</strong></div></li>';
+        str += `<li class="url">
+          <div class="urlbox">
+            <div class="name">${it.server_name}</div>
+            <div class="btn-delete">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"/>
+              </svg>
+            </div>
+          </div>
+          <div class="textEllipsis">
+              ${it.server_url}
+            </div>
+        </li>`;
       }); 
 
       str += '</ul>';
       document.getElementById('urls').innerHTML = str;
       //set delete button 
-      $("ul").on("click", "button", delete_server);
+      $("ul").on("click", ".btn-delete", delete_server);
 
       //save to chrome.storage
       chrome.storage.sync.set({
@@ -133,7 +145,7 @@ for(i in radios) {
       document.getElementById("server_url").placeholder = "Android FCM Token"
     }
     if (this.value === "iPhone") {
-      document.getElementById("server_url").placeholder = "Bark Push URL: https://day.app/xxxkeyxxxx/"
+      document.getElementById("server_url").placeholder = "Bark-revision Push URL: https://day.app/xxxkeyxxxx/"
     }
   }
 }
